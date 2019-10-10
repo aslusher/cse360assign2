67a3cb84af90f9bd2f0dc56085b14d30979df729
aslusher <56370860+aslusher@users.noreply.github.com>
2019-10-09 21:51:16 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -9,40 +9,49 @@
 package cse360assign2;
 
 /*
- * Public class AddingManchine has a total of 6 methods and one instance variable.
+ * Public class AddingManchine has a total of 6 methods and two instance variable.
  * */
 
 public class AddingMachine {
 
 	private int total;
-	
+	private String result;
+
 	//constructor of the AddingMachine class
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
-	}
-	
-	//get total returns an int and is a public method. No parameters are taken in.
-	public int getTotal () {
-		return 0;
-	}
-	
-	//add method has no return and is a public method. It takes in an int as a parameter.
-	public void add (int value) {
-		
-	}
-	
-	//subtract method has no return and is a public method. It takes in an int as a parameter
-	public void subtract (int value) {
-		
+		result = "0";
 	}
 
-	//toString method is a publid method with no parameters and returns a string.
+	//get total returns a current total and is a public method. No parameters are taken in.
+	public int getTotal () {
+		
+		return total;
+	}
+
+	//add method has no return and is a public method. It takes in an int as a parameter.
+	//adds the int parameter to the total instance variable.Adds to the result variable to use in toString method.
+	public void add (int value) {
+		
+		total = total + value;
+		result = result + " + " + value;
+	}
+
+	//subtract method has no return and is a public method. It takes in an int as a parameter
+	//subtracts the int parameter to the total instance variable.Adds to the result variable to use in toString method.
+	public void subtract (int value) {
+		
+		total = total - value; 
+		result = result + " - " + value;
+	}
+
+	//toString method is a public method with no parameters and returns string result.
 	public String toString () {
-		return "";
+		return result;
 	}
 
 	//clear method has no return and is a public method.No parameters are taken in.
 	public void clear() {
-	
+
 	}
 }
