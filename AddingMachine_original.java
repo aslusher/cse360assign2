6a180a1f22a16b76f8d276748af71a734cf5f837
Alexis Slusher <alexisslusher@icloud.com>
2019-10-09 21:58:53 -0700

    AddingMachine_original
    Original version of the script

---
+++ AddingMachine_original.java
@@ -0,0 +1,48 @@
+/*
+ * Author: Alexis Slusher
+ * Date:10/9/12
+ * Class ID:
+ * Assignment # 2
+ * */
+
+
+package cse360assign2;
+
+/*
+ * Public class AddingManchine has a total of 6 methods and one instance variable.
+ * */
+
+public class AddingMachine {
+
+	private int total;
+
+	//constructor of the AddingMachine class
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	//get total returns an int and is a public method. No parameters are taken in.
+	public int getTotal () {
+		return 0;
+	}
+
+	//add method has no return and is a public method. It takes in an int as a parameter.
+	public void add (int value) {
+
+	}
+
+	//subtract method has no return and is a public method. It takes in an int as a parameter
+	public void subtract (int value) {
+
+	}
+
+	//toString method is a publid method with no parameters and returns a string.
+	public String toString () {
+		return "";
+	}
+
+	//clear method has no return and is a public method.No parameters are taken in.
+	public void clear() {
+
+	}
+}
